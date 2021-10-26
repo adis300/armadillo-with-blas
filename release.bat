44dd372a29e4c23918a9733c9da90253cb9105cf
Disi A <adis300>
2021-10-26 12:04:34 +0800

    Update release script.

--- release.bat
+++ release.bat
@@ -2,12 +2,16 @@ rmdir /s /q release
 
 call unpack-deps.bat
 
+cd win
+call build-win.bat
+cd ..
 
-
-mkdir -p release\win\include
+mkdir -p release\win\include release\include
 
 xcopy /E win\openblas\include\openblas release\win\include
 xcopy win\openblas\lib\*.lib release\win
 
 xcopy /E win\armadillo\include release\win\include
 xcopy win\armadillo\lib\*.lib release\win
+
+xcopy /E release\win\include release\include
